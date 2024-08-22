58a91a26927e327b63426fad3b004501ab04e7ef
jp-cortes <code_alias@outlook.com>
2024-08-22 23:52:25 +0300

    showing info by parameter

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ recipes = [
      "ingredients": ["Tomato","cucumber","grounded white cheese", "smoked pepper", "olive oil", "apple cider vinnegar", "salt", "black pepper"],
      "preparation":"chop the tomatoe, cucumber and the smoked pepper in dices not bigger than 1cm. In a bowl mix the olive oil, vinnegar, salt and back pepper, to make a vinnagrette. Mix all the ingredients and put the cheese on top."
      },
+
+     {"id": 2,
+     "title": "bulgarian salad",
+     "ingredients": ["Tomato","cucumber","grounded white cheese", "smoked pepper", "olive oil", "apple cider vinnegar", "salt", "black pepper"],
+     "preparation":"chop the tomatoe, cucumber and the smoked pepper in dices not bigger than 1cm. In a bowl mix the olive oil, vinnegar, salt and back pepper, to make a vinnagrette. Mix all the ingredients and put the cheese on top."
+     },
 ]
 
 app = FastAPI()
@@ -21,3 +27,7 @@ def message():
 @app.get('/all', tags = ['recipes'])
 def get_recepies():
     return recipes
+
+@app.get('/recipe{id}', tags = ['recipe'])
+def get_recepies(id: int):
+    return id
