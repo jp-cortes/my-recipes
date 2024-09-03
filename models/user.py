c68ce7c9efb409474d7c6d6bf2613028bb1bdbba
jp-cortes <code_alias@outlook.com>
2024-09-03 23:59:08 +0300

    new table users

---
+++ models/user.py
@@ -0,0 +1,13 @@
+from config.database import Base
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key= True, autoincrement= True, unique= True, nullable= False)
+    name = Column(String, unique= True)
+    email= Column(String, nullable= False, unique= True)
+    password = Column(String, nullable= False)
